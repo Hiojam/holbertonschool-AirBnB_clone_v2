9c6b316b9f84df0239dfaf5368812340b7b1bd97
alemao51092 <aarielmartinez1111@gmail.com>
2023-08-14 09:46:22 -0700

    task2

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"Starts a Web Application"
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hbnb():
+    "Display 'Hello, HBNB!'"
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hello():
+    "Display 'HBNB!'"
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def C(text):
+    "Display 'c + text'"
+    return f"C {text}".replace("_", " ")
+
+
+
+if __name__ == "__main__":
+    """Starts a web app"""
+    app.run(debug=True, host="0.0.0.0", port=5000)
