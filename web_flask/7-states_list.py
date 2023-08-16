65505feb1446b4c5299b3fe819f909da155730c3
alemao51092 <aarielmartinez1111@gmail.com>
2023-08-16 06:41:30 -0700

    fixing errors

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""A script that starts a Flask web application"""
+"""List of states"""
 from flask import Flask, render_template
 from models import storage
 from models.state import State
@@ -8,6 +8,7 @@ app = Flask(__name__)
 
 @app.teardown_appcontext
 def app_teardown_appcontext(self):
+    "Close the session after each request"
     storage.close()
 
 
@@ -16,6 +17,6 @@ def states_list():
     return render_template("7-states_list.html",
                            states=storage.all(State).values())
 
+
 if __name__ == "__main__":
-    "Entry point"
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000)
